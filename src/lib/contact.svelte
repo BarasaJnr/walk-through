ab19e23b7a3fa4091931b9b76648513a426d8b0f
BarasaJnr <isaacbarasa714@gmail.com>
2024-06-10 16:18:13 +0300

    all commit

---
+++ src/lib/contact.svelte
@@ -0,0 +1,28 @@
+<div class="contact">
+    <h3>Contact Us</h3>
+    <p><strong>Start walkthrough</strong></p>
+    <p>Humming Place</p>
+    <p>Miami, AX 789</p>
+    <p>Phone: (000) 478976</p>
+    <p>Email:<a href="mailto:name@example.com">name@example.com</a></p>
+  </div>
+  
+  <style>
+    .contact {
+      grid-column: span 4;
+    }
+    h3 {
+      padding-bottom: 0.5em;
+      margin-bottom: 0.5em;
+      font-size: 2em;
+      font-weight: 500;
+      border-bottom: 1px solid #ddd;
+    }
+    a {
+      text-decoration: none;
+      color: var(--primary-color);
+    }
+    a:hover {
+      text-decoration: underline;
+    }
+  </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script>
-    import Navbar from "./Navbar.svelte";
-</script>
-
-<Navbar />
-<slot />
-
+    import "../../static/style.css";
+    import Footer from "$lib/footer.svelte";
+    import Navbar from "$lib/Navbar.svelte";
+  </script>
+  
+  <div class="app">
+    <Navbar />
+    <slot />
+    <Footer />
+  </div>
+  
+  <style>
+    .app {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+    }
+  </style>
