6bbf529b46aaed97dc9e86332d86870e8de1d48a
BarasaJnr <isaacbarasa714@gmail.com>
2024-06-19 13:05:14 +0300

    update pages

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,26 +1,39 @@
 <div class="hero">
-    <div class="container">
-      <h2>Empowering Young Minds</h2>
-      <p>
-        Welcome to ValleyGreen Academy, a premier educational institution in Nairobi, Kenya. Our mission is to provide a world-class education that empowers young minds to achieve their full potential.
-      </p>
-    </div>
+  <div class="container">
+    <img src="./images/School.jpg" alt="School.jpg" class="hero-image" />
+    <h2>Empowering Young Minds</h2>
+    <p>
+      Welcome to ValleyGreen Academy, a premier educational institution in
+      Nairobi, Kenya. Our mission is to provide a world-class education that
+      empowers young minds to achieve their full potential.
+    </p>
   </div>
-  
-  <style>
-    .hero {
-      padding: 5em 0;
-      background-color: var(--primary-color);
-      color: white;
-    }
-    h2 {
-      margin: 0;
-      font-size: 3.5em;
-      font-weight: 500;
-    }
-    p {
-      color: #ddd;
-      font-size: 1.1em;
-      line-height: 1.6;
-    }
-  </style>
+</div>
+
+<style>
+  .hero {
+    padding: 5em 0;
+    background-color: var(--primary-color);
+    color: white;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .hero-image {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    margin-bottom: 20px;
+  }
+  h2 {
+    margin: 0;
+    font-size: 3.5em;
+    font-weight: 500;
+  }
+  p {
+    color: #ddd;
+    font-size: 1.1em;
+    line-height: 1.6;
+  }
+</style>
